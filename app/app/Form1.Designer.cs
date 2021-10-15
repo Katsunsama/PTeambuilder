1aff53d7258f16fbd3f06738ee1c8527ccecef34
Sunny <92512805+Katsunsama@users.noreply.github.com>
2021-10-14 22:27:16 -0400

    Add files via upload

---
+++ app/app/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace app
+{
+    partial class PTeambuilder
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PTeambuilder));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "HomeView_Button";
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::app.Properties.Resources.A_Home_View;
+            this.button1.Location = new System.Drawing.Point(509, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(350, 550);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.HomeView_Button);
+            // 
+            // PTeambuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1352, 739);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1368, 778);
+            this.MinimumSize = new System.Drawing.Size(1368, 778);
+            this.Name = "PTeambuilder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PTeambuilder";
+            this.Load += new System.EventHandler(this.Home);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ app/app/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class PTeambuilder : Form
+    {
+        public PTeambuilder()
+        {
+            InitializeComponent();
+        }
+
+        private void Home(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HomeView_Button(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
